3d250964a354db8ef4b1b66247cbb556c697abbc
galleg0s <geraltGalleg0s@mail.ru>
2026-06-17 20:03:32 +0200

    Add Eslint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,63 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    settings: {},
+
+    rules: {
+      "no-console": "warn",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+
+      "import/no-duplicates": "error",
+      "import/first": "error",
+      "import/newline-after-import": "warn",
+
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+
+  prettier,
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "node_modules/**",
     "next-env.d.ts",
+    "public/**",
+    "next.config.js",
+    "postcss.config.js",
   ]),
 ]);
 
